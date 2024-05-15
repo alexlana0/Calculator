4d3277d256ae295706476217cc956658ccb4b3c3
Alex Lana <alex.application.usa@gmail.com>
2024-05-15 17:58:10 -0300

    preventing any error
    returning "Error" on display

--- ToDoList/script.js
+++ ToDoList/script.js
@@ -6,8 +6,14 @@ function addToDisplay(input){
 }
 
 function clearDisplay(){
+    display.value = "";
 }
 
 function calculate(){
+   try{ 
     display.value = eval(display.value);
+   }
+   catch(error){
+    display.value = "Error";
+   }
 }
